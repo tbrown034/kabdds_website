a2297d1b101ceb86537cf9263cc8bc146f34d5cc
tbrown034 <tbrown034@gmail.com>
2023-03-08 15:25:13 -0600

    header redo, all working now

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,41 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Button, Icon } from "semantic-ui-react";
 import HeaderCallBtn from "./HeaderCallBtn";
 
 const Header = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="md" bg="my-color">
-        <Container bg="my-container">
-          <Navbar.Brand bg="my-brand" href="#home">Keith Brown, D.D.S.</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              {/* Header links from BOOTSTRAP*/}
-              <Nav.Link href="/practice">Our Practice</Nav.Link>
-              <Nav.Link href="/financing">Financing</Nav.Link>
-              <Nav.Link href="/location">Location</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="/faqs">FAQs</Nav.Link>
-              <NavDropdown title="Services" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="/services">
-                  General Dentistry
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/services">
-                  Cosmetic Dentistry
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/services">
-                  Surgical Dentistry
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            {/* Header buttons from SEMANTIC UI*/}
-            <Nav>
-              <Button animated>
+       <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Keith Brown, D.D.S.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Financing</Nav.Link>
+            <Nav.Link href="#pricing">Location</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav.Link href="#pricing">FAQs</Nav.Link>
+            <NavDropdown title="Services" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">General Dentistry</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+              Cosmetic Dentistry
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+              Surgical Dentistry
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+          <Button animated>
                 <Button.Content circular visible>
                   <Icon name="calendar" /> Make an Appointment
                 </Button.Content>
@@ -44,12 +44,11 @@ const Header = () => {
                 </Button.Content>
               </Button>
               <HeaderCallBtn />
-              
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </>
   );
 };
 
